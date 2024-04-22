a8d7bbaefc8321f2dba5879195ee253bf6ceec1b
Muhammad Shoaib Shafique <shoaibxp463@gmail.com>
2024-04-22 18:19:16 +0500

    Players Stats

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
 from .video_utils import read_video,save_video
 from .bbox_utils import measure_distance,get_center_of_bbox,get_foot_position,get_closest_keypoint_index,get_height_of_bbox,measure_xy_distance
 from .conversions import convert_meters_to_pixel_distance,convert_pixel_distance_to_meters
+from .player_stats_drawer_utils import draw_player_stats
